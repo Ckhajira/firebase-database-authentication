1067c7f3ce9a73a34d5b8d79d4dbea2c427b4b61
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-12-12 12:30:30 +0300

    This commit adds picasso

--- app/src/main/java/com/example/techmarket/adapters/FirebasePhotoViewHolder.java
+++ app/src/main/java/com/example/techmarket/adapters/FirebasePhotoViewHolder.java
@@ -1,11 +1,26 @@
 package com.example.techmarket.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder{
+import com.example.techmarket.R;
+import com.example.techmarket.models.InterestingPhoto;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     View mView;
     Context mContext;
 
@@ -13,6 +28,46 @@ public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder{
         super(itemView);
         mView = itemView;
         mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
     }
+    public void bindRestaurant(InterestingPhoto photo){
+        ImageView imgPhoto = mView.findViewById(R.id.imgPhoto);
+        TextView txtPhotoName = mView.findViewById(R.id.txtPhotoName);
+
+
+        Picasso.get().load(restaurant.getImageUrl()).into(restaurantImageView);
+
+        nameTextView.setText((restaurant.getName()));
+        categoryTextView.setText(restaurant.getCategories().get(0).getTitle());
+        ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Business> restaurants = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    restaurants.add(snapshot.getValue(Business.class));
+                }
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("restaurants", Parcels.wrap(restaurants));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 
 }
