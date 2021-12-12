72ca86832b4aac2b73f9bba93986d5ecc534fe3d
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-12-12 12:12:53 +0300

    This commit adds the constructor to the FirebasePhotoViewHolder

--- app/src/main/java/com/example/techmarket/adapters/FirebasePhotoViewHolder.java
+++ app/src/main/java/com/example/techmarket/adapters/FirebasePhotoViewHolder.java
@@ -1,4 +1,18 @@
 package com.example.techmarket.adapters;
 
-public class FirebasePhotoViewHolder {
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder{
+    View mView;
+    Context mContext;
+
+    public FirebasePhotoViewHolder(View itemView){
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+    }
+
 }
