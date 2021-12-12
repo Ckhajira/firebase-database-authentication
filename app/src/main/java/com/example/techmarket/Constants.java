bffeb641cedab2654ca7795218680f5e691c300e
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-12-12 06:06:49 +0300

    This commit adds the child firebase node

--- app/src/main/java/com/example/techmarket/Constants.java
+++ app/src/main/java/com/example/techmarket/Constants.java
@@ -3,4 +3,5 @@ package com.example.techmarket;
 public class Constants {
     public static final String FLICKR_BASE_URL = "https://api.flickr.com/services/rest";
     public static final String FLICKR_API_KEY = BuildConfig.FLICKR_API_KEY;
+    public static final String FIREBASE_CHILD_PHOTOS = "photos";
 }
