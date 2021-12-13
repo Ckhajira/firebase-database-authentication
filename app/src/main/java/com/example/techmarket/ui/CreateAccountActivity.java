50a9b8e66f49b09e7dfd03c7c920bea257834a92
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-12-13 13:55:39 +0300

    This commit bind the resources in the CreateAccountActivity

--- app/src/main/java/com/example/techmarket/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/techmarket/ui/CreateAccountActivity.java
@@ -3,14 +3,41 @@ package com.example.techmarket.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.techmarket.R;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText)
+    EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    private void createNewUser(){
+        final String name = mName
     }
 }
