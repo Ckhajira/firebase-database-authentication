d40ee858aeaca5bc86108b5c560eba3efa8a91e7
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-12-13 05:41:15 +0300

    This commit completes the adding of the Firebase Recycler view

--- app/src/main/java/com/example/techmarket/adapters/FirebasePhotoViewHolder.java
+++ app/src/main/java/com/example/techmarket/adapters/FirebasePhotoViewHolder.java
@@ -1,162 +1,240 @@
-//package com.example.techmarket.adapters;
-//
-//import android.content.Context;
-//import android.content.Intent;
-//import android.view.View;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.example.techmarket.Constants;
-//import com.example.techmarket.R;
-//import com.example.techmarket.models.InterestingPhoto;
-//import com.example.techmarket.ui.InterestingPhotosActivity;
-//import com.example.techmarket.ui.SavedPhotosActivity;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-//import com.squareup.picasso.Picasso;
-//
-//import org.parceler.Parcels;
-//
-//import java.util.ArrayList;
-//
-//public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-//    View mView;
-//    Context mContext;
-//
-//    public FirebasePhotoViewHolder(View itemView){
-//        super(itemView);
-//        mView = itemView;
-//        mContext = itemView.getContext();
-//        itemView.setOnClickListener(this);
-//    }
-//    public void bindRestaurant(InterestingPhoto photo){
-//        ImageView imgPhoto = mView.findViewById(R.id.imgPhoto);
-//        TextView txtPhotoName = mView.findViewById(R.id.txtPhotoName);
-//
-//        photo = new InterestingPhoto();
-//
-//
-//
-//
-//        Picasso.get().load(photo.getPhotoURL()).into(imgPhoto);
-//
-//        txtPhotoName.setText((photo.getTitle()));
-//
-//    }
-//
-//    @Override
-//    public void onClick(View view) {
-//        final ArrayList<InterestingPhoto> photos = new ArrayList<>();
-//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PHOTOS);
-//        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-//                    photos.add(snapshot.getValue(InterestingPhoto.class));
-//                }
-////                int itemPosition = getLayoutPosition();
-//
-//                Intent intent = new Intent(mContext, SavedPhotosActivity.class);
-////                intent.putExtra("position", itemPosition + "");
-////                intent.putExtra("photos", Parcels.wrap(photos));
-//
-//                mContext.startActivity(intent);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//    }
-//
-//
-//
-//}
-
-
 package com.example.techmarket.adapters;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.TextView;
-        import androidx.annotation.NonNull;
-        import androidx.recyclerview.widget.RecyclerView;
 
-        import com.example.techmarket.R;
-        import com.example.techmarket.models.InterestingPhoto;
-        import com.firebase.ui.database.FirebaseRecyclerAdapter;
-        import com.firebase.ui.database.FirebaseRecyclerOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-// FirebaseRecyclerAdapter is a class provided by
-// FirebaseUI. it provides functions to bind, adapt and show
-// database contents in a Recycler View
-public class FirebasePhotoViewHolder extends FirebaseRecyclerAdapter<InterestingPhoto, FirebasePhotoViewHolder.PhotosViewholder> {
+import androidx.recyclerview.widget.RecyclerView;
 
-    public FirebasePhotoViewHolder(
-            @NonNull FirebaseRecyclerOptions<InterestingPhoto> options) {
-        super(options);
+import com.example.techmarket.Constants;
+import com.example.techmarket.R;
+import com.example.techmarket.models.InterestingPhoto;
+import com.example.techmarket.ui.AllProductsActivity;
+import com.example.techmarket.ui.SavedPhotosActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+////package com.example.techmarket.adapters;
+////
+////import android.content.Context;
+////import android.content.Intent;
+////import android.view.View;
+////import android.widget.ImageView;
+////import android.widget.TextView;
+////
+////import androidx.recyclerview.widget.RecyclerView;
+////
+////import com.example.techmarket.Constants;
+////import com.example.techmarket.R;
+////import com.example.techmarket.models.InterestingPhoto;
+////import com.example.techmarket.ui.InterestingPhotosActivity;
+////import com.example.techmarket.ui.SavedPhotosActivity;
+////import com.google.firebase.database.DataSnapshot;
+////import com.google.firebase.database.DatabaseError;
+////import com.google.firebase.database.DatabaseReference;
+////import com.google.firebase.database.FirebaseDatabase;
+////import com.google.firebase.database.ValueEventListener;
+////import com.squareup.picasso.Picasso;
+////
+////import org.parceler.Parcels;
+////
+////import java.util.ArrayList;
+////
+////public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+////    View mView;
+////    Context mContext;
+////
+////    public FirebasePhotoViewHolder(View itemView){
+////        super(itemView);
+////        mView = itemView;
+////        mContext = itemView.getContext();
+////        itemView.setOnClickListener(this);
+////    }
+////    public void bindRestaurant(InterestingPhoto photo){
+////        ImageView imgPhoto = mView.findViewById(R.id.imgPhoto);
+////        TextView txtPhotoName = mView.findViewById(R.id.txtPhotoName);
+////
+////        photo = new InterestingPhoto();
+////
+////
+////
+////
+////        Picasso.get().load(photo.getPhotoURL()).into(imgPhoto);
+////
+////        txtPhotoName.setText((photo.getTitle()));
+////
+////    }
+////
+////    @Override
+////    public void onClick(View view) {
+////        final ArrayList<InterestingPhoto> photos = new ArrayList<>();
+////        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PHOTOS);
+////        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+////            @Override
+////            public void onDataChange(DataSnapshot dataSnapshot) {
+////                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+////                    photos.add(snapshot.getValue(InterestingPhoto.class));
+////                }
+//////                int itemPosition = getLayoutPosition();
+////
+////                Intent intent = new Intent(mContext, SavedPhotosActivity.class);
+//////                intent.putExtra("position", itemPosition + "");
+//////                intent.putExtra("photos", Parcels.wrap(photos));
+////
+////                mContext.startActivity(intent);
+////            }
+////
+////            @Override
+////            public void onCancelled(DatabaseError databaseError) {
+////
+////            }
+////        });
+////    }
+////
+////
+////
+////}
+//
+//
+//package com.example.techmarket.adapters;
+//        import android.view.LayoutInflater;
+//        import android.view.View;
+//        import android.view.ViewGroup;
+//        import android.widget.TextView;
+//        import androidx.annotation.NonNull;
+//        import androidx.recyclerview.widget.RecyclerView;
+//
+//        import com.example.techmarket.R;
+//        import com.example.techmarket.models.InterestingPhoto;
+//        import com.firebase.ui.database.FirebaseRecyclerAdapter;
+//        import com.firebase.ui.database.FirebaseRecyclerOptions;
+//
+//// FirebaseRecyclerAdapter is a class provided by
+//// FirebaseUI. it provides functions to bind, adapt and show
+//// database contents in a Recycler View
+//public class FirebasePhotoViewHolder extends FirebaseRecyclerAdapter<InterestingPhoto, FirebasePhotoViewHolder.PhotosViewholder> {
+//
+//    public FirebasePhotoViewHolder(
+//            @NonNull FirebaseRecyclerOptions<InterestingPhoto> options) {
+//        super(options);
+//    }
+//
+//        // Function to bind the view in Card view(here
+//    // "person.xml") iwth data in
+//    // model class(here "person.class")
+//    @Override
+//    protected void
+//    onBindViewHolder(@NonNull PhotosViewholder holder,
+//                     int position, @NonNull InterestingPhoto model)
+//    {
+//
+//        // Add firstname from model class (here
+//        // "person.class")to appropriate view in Card
+//        // view (here "person.xml")
+//      //  holder.firstname.setText(model.getFirstname());
+//
+//        holder.title.setText(model.getTitle());
+//        // Add lastname from model class (here
+//        // "person.class")to appropriate view in Card
+//        // view (here "person.xml")
+//        holder.date.setText(model.getDateTaken());
+//
+//        // Add age from model class (here
+//        // "person.class")to appropriate view in Card
+//        // view (here "person.xml")
+//      //  holder.age.setText(model.getAge());
+//    }
+//
+//    // Function to tell the class about the Card view (here
+//    // "person.xml")in
+//    // which the data will be shown
+//    @NonNull
+//    @Override
+//    public PhotosViewholder
+//    onCreateViewHolder(@NonNull ViewGroup parent,
+//                       int viewType)
+//    {
+//        View view
+//                = LayoutInflater.from(parent.getContext())
+//                .inflate(R.layout.activity_saved_photos, parent, false);
+//        return new FirebasePhotoViewHolder.PhotosViewholder(view);
+//    }
+//
+//    // Sub Class to create references of the views in Crad
+//    // view (here "person.xml")
+//    class PhotosViewholder
+//            extends RecyclerView.ViewHolder {
+//        TextView title, date, url;
+//        public PhotosViewholder(@NonNull View itemView)
+//        {
+//            super(itemView);
+//
+//            title = itemView.findViewById(R.id.title);
+//            date = itemView.findViewById(R.id.date);
+//            url = itemView.findViewById(R.id.url);
+//        }
+//    }
+//}
+//
+public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    View mView;
+    Context mContext;
+
+    public FirebasePhotoViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
     }
 
-        // Function to bind the view in Card view(here
-    // "person.xml") iwth data in
-    // model class(here "person.class")
+    public void bindPhoto(InterestingPhoto photo) {
+        ImageView photoImageView = (ImageView) mView.findViewById(R.id.imgPhoto);
+        TextView titleTextView = (TextView) mView.findViewById(R.id.title);
+        TextView dateTextView = (TextView) mView.findViewById(R.id.date);
+        TextView urlTextView = (TextView) mView.findViewById(R.id.url);
+
+       // Picasso.get().load(restaurant.getImageUrl()).into(restaurantImageView);
+
+        titleTextView.setText(photo.getTitle());
+        dateTextView.setText(photo.getDateTaken());
+        urlTextView.setText(photo.getPhotoURL());
+    }
+
     @Override
-    protected void
-    onBindViewHolder(@NonNull PhotosViewholder holder,
-                     int position, @NonNull InterestingPhoto model)
-    {
+    public void onClick(View view) {
+        final ArrayList<InterestingPhoto> photos = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PHOTOS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
 
-        // Add firstname from model class (here
-        // "person.class")to appropriate view in Card
-        // view (here "person.xml")
-      //  holder.firstname.setText(model.getFirstname());
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    photos.add(snapshot.getValue(InterestingPhoto.class));
+                }
 
-        holder.title.setText(model.getTitle());
-        // Add lastname from model class (here
-        // "person.class")to appropriate view in Card
-        // view (here "person.xml")
-        holder.date.setText(model.getDateTaken());
+                int itemPosition = getLayoutPosition();
 
-        // Add age from model class (here
-        // "person.class")to appropriate view in Card
-        // view (here "person.xml")
-      //  holder.age.setText(model.getAge());
-    }
+                Intent intent = new Intent(mContext, SavedPhotosActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("photos", Parcels.wrap(photos));
 
-    // Function to tell the class about the Card view (here
-    // "person.xml")in
-    // which the data will be shown
-    @NonNull
-    @Override
-    public PhotosViewholder
-    onCreateViewHolder(@NonNull ViewGroup parent,
-                       int viewType)
-    {
-        View view
-                = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_saved_photos, parent, false);
-        return new FirebasePhotoViewHolder.PhotosViewholder(view);
-    }
+                mContext.startActivity(intent);
+            }
 
-    // Sub Class to create references of the views in Crad
-    // view (here "person.xml")
-    class PhotosViewholder
-            extends RecyclerView.ViewHolder {
-        TextView title, date, url;
-        public PhotosViewholder(@NonNull View itemView)
-        {
-            super(itemView);
-
-            title = itemView.findViewById(R.id.title);
-            date = itemView.findViewById(R.id.date);
-            url = itemView.findViewById(R.id.url);
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
-
--- app/src/main/java/com/example/techmarket/ui/SavedPhotosActivity.java
+++ app/src/main/java/com/example/techmarket/ui/SavedPhotosActivity.java
@@ -1,29 +1,174 @@
+//package com.example.techmarket.ui;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.os.Bundle;
+//
+//import com.example.techmarket.Constants;
+//import com.example.techmarket.R;
+//import com.example.techmarket.adapters.FirebasePhotoViewHolder;
+//import com.example.techmarket.models.InterestingPhoto;
+//import com.firebase.ui.database.FirebaseRecyclerAdapter;
+//import com.firebase.ui.database.FirebaseRecyclerOptions;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//import butterknife.BindView;
+//
+//public class SavedPhotosActivity extends AppCompatActivity {
+//    private DatabaseReference mPhotoReference;
+//    private RecyclerView recyclerView;
+//    private FirebasePhotoViewHolder adapter;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_recycler2);
+//
+//        // Create a instance of the database and get
+//        // its reference
+//        mPhotoReference = FirebaseDatabase.getInstance().getReference();
+//
+//        recyclerView = findViewById(R.id.recycler2);
+//
+//        // To display the Recycler view linearly
+//        recyclerView.setLayoutManager(
+//                new LinearLayoutManager(this));
+//        // It is a class provide by the FirebaseUI to make a
+//        // query in the database to fetch appropriate data
+//        FirebaseRecyclerOptions<InterestingPhoto> options
+//                = new FirebaseRecyclerOptions.Builder<InterestingPhoto>()
+//                .setQuery(mPhotoReference, InterestingPhoto.class)
+//                .build();
+//        // Connecting object of required Adapter class to
+//        // the Adapter class itself
+//        adapter = new FirebasePhotoViewHolder(options);
+//        // Connecting Adapter class with the Recycler view*/
+//        recyclerView.setAdapter(adapter);
+//    }
+//
+//    // Function to tell the app to start getting
+//    // data from database on starting of the activity
+//
+//    @Override protected void onStart()
+//    {
+//        super.onStart();
+//        adapter.startListening();
+//    }
+//
+//    // Function to tell the app to stop getting
+//    // data from database on stopping of the activity
+//    @Override protected void onStop()
+//    {
+//        super.onStop();
+//        adapter.stopListening();
+//    }
+////    private FirebaseRecyclerAdapter<InterestingPhoto, FirebasePhotoViewHolder> mFirebaseAdapter;
+////
+////    @BindView(R.id.)
+////    RecyclerView m;
+////    @BindView(R.id.errorTextView) TextView mErrorTextView;
+////    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+////    @Override
+////    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_saved_photos);
+////    }
+//}
 package com.example.techmarket.ui;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
+import com.example.techmarket.Constants;
 import com.example.techmarket.R;
 import com.example.techmarket.adapters.FirebasePhotoViewHolder;
 import com.example.techmarket.models.InterestingPhoto;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SavedPhotosActivity extends AppCompatActivity {
     private DatabaseReference mPhotoReference;
     private FirebaseRecyclerAdapter<InterestingPhoto, FirebasePhotoViewHolder> mFirebaseAdapter;
 
-    @BindView(R.id.)
-    RecyclerView m;
-    @BindView(R.id.errorTextView) TextView mErrorTextView;
-    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+    @BindView(R.id.recycler2)
+    RecyclerView mRecyclerView;
+//    @BindView(R.id.errorTextView)
+//    TextView mErrorTextView;
+//    @BindView(R.id.progressBar)
+//    ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_photos);
+        setContentView(R.layout.activity_recycler2);
+        ButterKnife.bind(this);
+
+        mPhotoReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PHOTOS);
+        setUpFirebaseAdapter();
+//        hideProgressBar();
+        showRestaurants();
     }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<InterestingPhoto> options =
+                new FirebaseRecyclerOptions.Builder<InterestingPhoto>()
+                        .setQuery(mPhotoReference, InterestingPhoto.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<InterestingPhoto, FirebasePhotoViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebasePhotoViewHolder firebaseRestaurantViewHolder, int position, @NonNull InterestingPhoto photo) {
+                firebaseRestaurantViewHolder.bindPhoto(photo);
+            }
+
+            @NonNull
+            @Override
+            public FirebasePhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_saved_photos, parent, false);
+                return new FirebasePhotoViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+
+    private void showRestaurants() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+//    private void hideProgressBar() {
+//        mProgressBar.setVisibility(View.GONE);
+//    }
 }
